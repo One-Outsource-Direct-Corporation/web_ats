8e53c2c644fef8ab9ce0c41ce531a5ec03f22315
jmcvz <jmchave15360@gmail.com>
2025-06-11 11:29:12 +0800

    jmcc-login-module base model

--- src/assets/components/pages/Login.tsx
+++ src/assets/components/pages/Login.tsx
@@ -1,1 +1,34 @@
-// test
+import React from "react";
+import "./Login.css";
+import oodc from './images/oodc.svg';
+
+const Login: React.FC = () => {
+  return (
+    <div className="login-container">
+  <div className="login-wrapper">
+    <div className="login-left">
+      <img src={oodc} alt="OODC Logo" className="login-logo" />
+      <h1 className="login-title">One Outsource</h1>
+      <h2 className="login-subtitle">Applicant Tracking System</h2>
+    </div>
+    <div className="login-right">
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+  );
+};
+
+export default Login;
