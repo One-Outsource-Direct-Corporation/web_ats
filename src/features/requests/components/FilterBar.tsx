5b038718017d56c70721088697c8e1effbf870ce
JGabZXC <jlopezgab25@gmail.com>
2025-11-16 16:43:34 +0800

    Moved the add new position in Request component

--- src/features/requests/components/FilterBar.tsx
+++ src/features/requests/components/FilterBar.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/shared/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/shared/components/ui/dialog";
 import { Input } from "@/shared/components/ui/input";
 import {
   Select,
@@ -6,6 +15,8 @@ import {
   SelectContent,
   SelectItem,
 } from "@/shared/components/ui/select.tsx";
+import { User, Users2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 function FilterBar({
   filters,
   setFilters,
@@ -25,6 +36,8 @@ function FilterBar({
     order_by: string;
   }) => void;
 }) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-wrap justify-between items-center gap-4">
       <Input placeholder="Search positions..." className="w-64" />
@@ -129,6 +142,51 @@ function FilterBar({
             <SelectItem value="asc">Oldest To Latest</SelectItem>
           </SelectContent>
         </Select>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="bg-blue-600 hover:bg-blue-700 cursor-pointer">
+              Add New Position
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-blue-700 text-sm text-center font-semibold">
+                SELECT TYPE OF HIRING
+              </DialogTitle>
+              <DialogDescription hidden aria-hidden>
+                Choose between Internal Hiring or Client
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex justify-center gap-12 mt-6">
+              {/* Internal Hiring */}
+              <div
+                className="flex flex-col items-center space-y-2 cursor-pointer group"
+                onClick={() => navigate("/prf")}
+              >
+                <div className="w-16 h-16 rounded-full border border-gray-500 text-gray-600 flex items-center justify-center group-hover:border-blue-500 group-hover:text-blue-500">
+                  <User className="w-6 h-6" />
+                </div>
+                <span className="text-sm text-gray-600 font-medium group-hover:text-blue-500">
+                  Internal Hiring
+                </span>
+              </div>
+
+              {/* Client */}
+              <div
+                className="flex flex-col items-center space-y-2 cursor-pointer group"
+                onClick={() => navigate("/positions/create-new-position")}
+              >
+                <div className="w-16 h-16 rounded-full border border-gray-500 text-gray-600 group-hover:border-blue-500 group-hover:text-blue-500 flex items-center justify-center">
+                  <Users2 className="w-6 h-6" />
+                </div>
+                <span className="text-sm text-gray-600 group-hover:text-blue-500 font-medium">
+                  Client
+                </span>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
